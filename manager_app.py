5cdcec93ad35ad8749a070a0e20a2fe69938b554
nastya-exe <lena.krytko@mail.ru>
2025-09-04 17:00:04 +0300

    change manager_app.py

--- manager_app.py
+++ manager_app.py
@@ -1,40 +1,8 @@
 import flet as ft
 import os
+from datetime import datetime
 
-
-def make_panel(path, level=0):
-    controls = []
-    for entry in os.scandir(path):
-        if entry.is_dir():
-            controls.append(
-                ft.ExpansionPanelList(
-                    elevation=8,
-                    controls=[
-                        ft.ExpansionPanel(
-                            header=ft.Row(
-                                controls=[
-                                    ft.Icon(ft.Icons.FOLDER),
-                                    ft.Text(entry.name),
-                                ]
-                            ),
-                            content=ft.Column(make_panel(entry.path, level + 1))
-                        )
-                    ]
-                )
-            )
-
-    for entry in os.scandir(path):
-        if entry.is_file():
-            controls.append(
-                ft.Row(
-                    controls=[
-                        ft.Icon(ft.Icons.ATTACH_FILE_SHARP),
-                        ft.Text(entry.name)
-                    ],
-                    spacing=5
-                )
-            )
-    return controls
+from functions_app import get_size_dir, make_panel, del_file_dir, checkbox_paths, convert_size
 
 
 def main(page: ft.Page):
@@ -45,22 +13,11 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    def validate(e):
-        if all([dir_name.value]):
-            btn_dir.disabled = False
-        else:
-            btn_dir.disabled = True
-
-        page.update()
-
-    def write_to_main_txt(e):
-        with open("main.txt", "w", encoding="utf-8") as f:
-            f.write(dir_name.value)
-        page.update()
-
-
-    dir_name = ft.TextField(label='Название папки', width=200, on_change=validate)
     panel = ft.Column()
+
+    def update_tree():
+        make_tree(None)
+        page.update()
 
     def make_tree(e):
         folder_name = dir_name.value
@@ -77,26 +34,89 @@ def main(page: ft.Page):
                     path = os.path.join(root, folder_name)
                     break
 
-        panel.controls=[
+        panel.controls = [
             ft.ExpansionPanelList(
+                expanded_header_padding=0,
                 controls=[
                     ft.ExpansionPanel(
-                        header=ft.Text(folder_name),
-                        content=ft.Column(make_panel(path))
+                        can_tap_header=True,
+                        expanded=True,
+                        header=ft.Row(
+                            controls=[
+                                ft.Row(
+                                    scroll=ft.ScrollMode.AUTO,
+                                    width=300,
+                                    spacing=5,
+                                    controls=[
+                                        ft.Icon(ft.Icons.FOLDER),
+                                        ft.Text(folder_name),
+                                    ]
+                                ),
+                                ft.Container(
+                                    content=ft.Text(convert_size(get_size_dir(folder_name))),
+                                    alignment=ft.alignment.center,
+                                    width=600,
+                                ),
+                                ft.Container(
+                                    content=ft.Text(datetime.fromtimestamp(os.path.getctime(path)).date()),
+                                    width=100,
+                                ),
+                            ]
+                        ),
+                        content=ft.Column(make_panel(path, update_tree=update_tree))
                     )
                 ]
             )
         ]
-
         page.update()
 
-    btn_dir = ft.ElevatedButton('Выбрать', icon=ft.Icons.CHECK, width=200, disabled=True, on_click=make_tree)#on_click=write_to_main_txt, disabled=True)
+    def del_file_dir_checkbox(e):
+        for item in checkbox_paths.items():
+            if item[1].value:
+                del_file_dir(item[0])
+        make_tree(e)
+        page.update()
+
+    def validate_btn_dir(e):
+        if all([dir_name.value]):
+            btn_dir.disabled = False
+        else:
+            btn_dir.disabled = True
+        page.update()
+
+    def validate_btn_checkbox(e):
+        pass
+
+
+    def write_to_main_txt(e):
+        with open("main.txt", "w", encoding="utf-8") as f:
+            f.write(dir_name.value)
+        make_tree(e)
+        btn_checkbox.visible = True
+        page.update()
+
+    table_title = ft.Row(
+        controls=[
+            ft.Container(content=ft.Text('Название файла'), width=570),
+            ft.Container(content=ft.Text('Вес файла'), width=300),
+            ft.Container(content=ft.Text('Дата создания файла'), width=450)
+        ]
+    )
+
+    dir_name = ft.TextField(label='Название папки', width=200, on_change=validate_btn_dir)
+    btn_dir = ft.ElevatedButton('Выбрать', icon=ft.Icons.CHECK, width=200, disabled=True, on_click=write_to_main_txt)
+    btn_checkbox = ft.Container(
+        content=ft.ElevatedButton('Удалить', icon=ft.Icons.DELETE, width=200, on_click=del_file_dir_checkbox, disabled=True),
+        visible=False, alignment=ft.alignment.center_right
+    )
 
     page.add(ft.Column([
-        ft.Text('Введите название папки в которой будет происходить поиск файлов'),
+        ft.Text('Введите название директории, в которой будет происходить поиск файлов'),
         dir_name,
         btn_dir,
-        panel
+        table_title,
+        panel,
+        btn_checkbox,
     ]))
 
 
